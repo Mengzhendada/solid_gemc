ce03ca560d31df731262c4d75f571fdcf67da06e
tianye8001 <tianye8001@gmail.com>
2023-06-06 10:50:12 -0400

    change the directory of the input files

--- prototype/ec_pro/HallC2022/submit_ifarmjlabs/readme.C
+++ prototype/ec_pro/HallC2022/submit_ifarmjlabs/readme.C
@@ -2,3 +2,4 @@
 ./LongRun_sim
 1000  //the start run number
 2000  //the last run number
+// submit BeamOnTarget files: uncomment  make_beam_LD2 in the ../geometry_18deg/beamline_geometry.pl file to include target 
